ed95b83b11259b68b63fc45f0656cbd7739f29d6
Awalone01 <sashaneiv01@yandex.ru>
2022-03-19 16:17:28 +0300

    Commit 4 (2.0_course-work)

--- src/main/java/pro/sky2/_courswork/Service/Impl/JavaQuestionService.java
+++ src/main/java/pro/sky2/_courswork/Service/Impl/JavaQuestionService.java
@@ -3,6 +3,8 @@ package pro.sky2._courswork.Service.Impl;
 import org.springframework.stereotype.Service;
 import pro.sky2._courswork.Service.QuestionService;
 import pro.sky2._courswork.data.Question;
+import pro.sky2._courswork.exception.EmployeeExistException;
+import pro.sky2._courswork.exception.EmployeeNotFoundException;
 
 import java.util.HashSet;
 import java.util.Random;
@@ -19,6 +21,10 @@ public class JavaQuestionService implements QuestionService {
     @Override
     public Question addQuestion(String question, String answer) {
         Question addingQuestion = new Question(question, answer);
+
+        if (questions.contains(addingQuestion)) {
+            throw new EmployeeExistException("Этот сотрудник уже добавлен");
+        }
         questions.add(addingQuestion);
         return addingQuestion;
     }
@@ -30,9 +36,11 @@ public class JavaQuestionService implements QuestionService {
 
     @Override
     public Question removeQuestion(String question, String answer) {
-
         Question removingQuestion = new Question(question, answer);
 
+        if (!questions.contains(removingQuestion)) {
+            throw new EmployeeNotFoundException("Этот сотрудник не найден");
+        }
         return removingQuestion;
     }
 
